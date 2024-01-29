1a0a8862e6912349da1f52b086e9fe4ef9f86fd9
NishaRaiyani007 <nishuraiyani7@gmail.com>
2024-01-29 20:22:40 +0530

    Done

---
+++ pro-1  largest number, smallest num and sum of all.py
@@ -0,0 +1,7 @@
+l=[]
+num=int(input('how many numbers'))
+for n in range(num):
+    number=int(input("Enter number"))
+    l.append(numbers)
+print("maximum element:",max(l),"\nminimum element:",min(l))    
+  
