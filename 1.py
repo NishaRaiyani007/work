9e721d8d0b87ac19c5722d64bca2fc8c6c4db029
NishaRaiyani007 <nishuraiyani7@gmail.com>
2024-02-08 11:50:56 +0530

    Done

---
+++ 1.py
@@ -0,0 +1,76 @@
+# print("hello")
+
+#chack datatype
+"""a=1234567.89
+print("A :",a)
+print(type(a))"""
+
+#addition
+"""a=10
+b=20
+c=a+b
+print("Adition : ",c)"""
+
+#user type value 
+"""a=int(input("enter A : "))
+b=int(input("enter B : "))
+c=a+b
+print("Adition : ",c)"""
+
+#odd and even number
+"""a=int(input("Enter A : "))
+
+if a%2==0:
+    print("A is Even Number")
+
+else:
+    print("A is odd Number")"""
+
+#max and min value
+"""a=int(input("Enter A : "))
+b=int(input("Enter B : "))
+c=int(input("Enter C : "))
+
+if a>b:
+    if a>c:
+        print("A is Greater")
+    else:
+        print("C is Greater")
+elif b>c:
+    print("B is Greater")
+else:
+    print("C is Greater")"""
+
+#ladderif (student markcit)
+"""sname=input("Enter Student Name : ")
+rno=int(input("Enter Roll No : "))
+s1=int(input("Enter Marks Of Subject 1 :"))
+s2=int(input("Enter Marks of Subject 2 :"))
+s3=int(input("Enter Marks of Subject 3 :"))
+
+total=s1+s2+s3
+per=total/3
+
+print("Student Name : ",sname)
+print("Roll No : ",rno)
+print("Total : ",total)
+print("percentage : ",per)
+
+if per>=70:
+    print("Distiction")
+elif per>=60:
+    print("First Class")
+elif per>=50:
+    print("Second class")
+elif per>=40:
+    print("pass class")
+else:
+    print("Fail")"""
+
+
+
+
+
+
+
+
