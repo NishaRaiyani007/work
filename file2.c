c9762d070a00f659a17e7004d051b3df734a265c
NishaRaiyani007 <nishuraiyani7@gmail.com>
2024-02-18 18:13:05 +0530

    Done

---
+++ file2.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+int main ()
+{
+    short a;
+    long b;
+    long long c;
+    long double d;
+    printf("size of sort = %d bytes\n", sizeof(a));
+    printf("size of long = %d bytes\n", sizeof(b));
+    printf("size of long long = %d bytes\n", sizeof(c));
+    printf("size of long double = %d bytes\n", sizeof(d));
+    return 0;
+}
+#include <stdio.h>
+int main(){
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of sort = %d bytes\n", sizeof(a));
+    printf("size of long = %d bytes\n", sizeof(b));
+    printf("size of long long = %d bytes\n", sizeof(c));
+    printf("size of long double = %d bytes\n", sizeof(d));
+
+}
+#include <stdio.h>
+int main(){
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of sort = %d bytes\n", sizeof(a));
+    printf("size of long = %d bytes\n", sizeof(c));
+    printf("size of long long = %d bytes\n", sizeof(c));
+}
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of sort = %d bytes\n", size(a));
+    printf("size of long = %d bytes\n", size(b));
+    printf("size of long = %d bytes\n", size(c);
+    printf("size of long = %d bytes\n", size(d));
+
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of sort = %d bytest\n", size(a));
+    printf("size of sort = %d bytest\n", size(a));
+    printf("size of sort = %d bytest\n", size(c));
+    printf("size of sort = %d bytest\n", size(d));
+    printf("size of sort = %d bytest\n", size(b));
+    printf("size of sort = %d bytest\n", size(a));
+
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of short = %d bytest\n", size(a));
+    printf("size of sort = %d bytest\n", size(a));
+    printf("size of sort = %d bytest\n", size(b));
+    printf("size of sort = %d bytest\n", size(d));
+
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of short = %d bytest\n", size(a));
+    printf("size of short = %d bytest\n", size(b));
+    printf("size of short = %d bytest\n", size(c));
+    printf("size of short = %d bytest\n", size(d));
+
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of short = %d bytest\n", size(a));
+    printf("size of short = %d bytest\n", size(b));
+    printf("size of short = %d bytest\n", size(c));
+    printf("size of short = %d bytest\n", size(d));
+
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("size of short = %d bytest\n", size(b));
+    printf("size of short = %d bytest\n", size(b));
+    printf("size of short = %d bytest\n", size(c));
+    printf("size of short = %d bytest\n", size(d));
+    printf("size of short = %d bytest\n", size(a));
+
+#include <stdio.h>
+int main()
+    short a;
+    long b;
+    long long c;
+    long double d;
+    
+             
+
