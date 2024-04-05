c543991151b4faca4e5c9ae0fbcbe9464418ad70
NishaRaiyani007 <nishuraiyani7@gmail.com>
2024-04-05 13:46:43 +0530

    Done

---
+++ struct.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+struct myfun{
+	int a;
+	int b;
+};
+main()
+{
+	struct myfun s1;
+	printf("Enter The Number :");
+	scanf("%d",&s1.a);
+	printf("Enter The Number :");
+	scanf("%d",&s1.b);
+}
